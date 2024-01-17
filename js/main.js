4c6d80e418cc3eecc56476b6ce5794463b5c0f90
l19200895 <155929147+l19200895@users.noreply.github.com>
2024-01-17 16:23:07 -0600

    Proyecto Terminado

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// alert("Hola Mundo!!");
+
+let ip=document.getElementById("ip");
+let pais=document.getElementById("pais");
+let continente=document.getElementById("continente");
+let zona_horaria=document.getElementById("zona_horaria");
+
+const solicitudAPI = () => {
+ // Hacer una petición para un usuario con ID especifico
+axios.get("https://progwebform.000webhostapp.com/php-geoip-api/index.php")
+  .then(function (response) {
+    // manejar respuesta exitosa
+    console.log(response.data);
+    ip.innerHTML = response.data.ip;
+    pais.innerHTML = response.data.pais;
+    continente.innerHTML = response.data.continente;
+    zona_horaria.innerHTML = response.data.zona_horaria;
+  })
+  .catch(function (error) {
+    // manejar error
+    console.log(error);
+  })
+  .finally(function () {
+    // siempre sera executado
+  }); 
+};
+
+// Llama al evento load cada vez que se actualiza la pagina
+// y llama a la funcion solicitudAPI que tiene la rutina de 
+// llamar a la API desde axios
+window.onload = solicitudAPI;
